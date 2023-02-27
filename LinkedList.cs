0263b4f920bb4877fcfff9cb2a5bec392178b0bf
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-27 20:35:17 +0530

    Add : Added LinkedList.cs File

--- LinkedList.cs
+++ LinkedList.cs
@@ -29,7 +29,7 @@ namespace LinkedListDemo
             Console.WriteLine("{0} inserted into linked list", node.data);
         }
 
-        public void display()
+        public void Display()
         {
             Node temp = this.head;
             if (temp == null)
@@ -42,6 +42,40 @@ namespace LinkedListDemo
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
             }
+        }
+        public Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid Position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                {
+                    Console.WriteLine("Position out of range");
+
+                }
+            }
+            Console.WriteLine("After Inserting value in Linkedlist ");
+
+
+            return head;
         }
     }
 }
