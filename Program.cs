fd7ee176f1e1378930e37385f50eabe4473d075f
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-27 20:42:35 +0530

    Add : Added Program.cs File

--- Program.cs
+++ Program.cs
@@ -9,16 +9,12 @@ namespace LinkedListDemo
         {
 
             LinkedList linked = new LinkedList();
-            linked.Append(56); // add a element at last postion Append is nothinf act as Add () method
-            linked.Append(30);
-            linked.Append(70);
+            linked.Add(56); // add a elemt at last postion Append is nothinf act as Add () method
+            linked.Add(30);
+            linked.Add(70);
             linked.Display();
-            linked.InsertAtParticularPosition(2, 40);
-            linked.Display();
-            linked.RemoveFirstNode();
-            linked.Display();
-            linked.RemoveLastNode();
-            linked.Display();
+            int a = linked.Search(30);
+            Console.WriteLine("index position of element is :" + a);
         }
     }
 }
