337a96088ce965a172f9a9d7fd68797a8d02b87f
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-27 20:24:48 +0530

    Add : Added  Program.cs File

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedListDemo
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            
+            LinkedList link = new LinkedList();
+            link.Add(30);
+            link.Add(80);
+            link.Add(50);
+            link.Add(10);
+
+            //link.display();
+        }
+    }
+}
